8ed1e21c6e494cf0a9450d88da61562159a8baca
Zhu Shuang <30600875+breakersol@users.noreply.github.com>
2024-04-25 12:09:24 +0800

    Extended file operation in Serialization.
    Extended file operation in Serialization.

--- ActivityFramework/Src/Components/TA_Serialization.h
+++ ActivityFramework/Src/Components/TA_Serialization.h
@@ -26,11 +26,11 @@ namespace CoreAsync
 
     enum class OperationType
     {
-        Read,
-        Write
+        Input,
+        Output
     };
 
-    template <SerializationType SType = SerializationType::BinaryFile>
+    template <SerializationType SType = SerializationType::BinaryFile, OperationType OType = OperationType::Output>
     class TA_Serialization
     {
         template <typename P>
@@ -46,12 +46,7 @@ namespace CoreAsync
 
         TA_Serialization(const std::string &path)
         {
-            m_fileSream.open(path, std::ios::binary);
-            if (!m_fileSream || !m_fileSream.is_open())
-            {
-                CoreAsync::TA_CommonTools::debugInfo("Cannot open the file for writing.\n");
-                return;
-            }
+            openFile(path);
         }
 
         ~TA_Serialization()
@@ -67,28 +62,21 @@ namespace CoreAsync
 
         void setFilePath(const std::string &path)
         {
-            if(m_fileSream.is_open())
-            {
-                m_fileSream.close();
-            }
-            m_fileSream.open(path, std::ios::binary);
-            if (!m_fileSream || !m_fileSream.is_open())
-            {
-                CoreAsync::TA_CommonTools::debugInfo("Cannot open the file for writing.\n");
-                return;
-            }
+            openFile(path);
         }
 
         template <CustomType T>
         TA_Serialization & operator << (const T &t)
         {
-            callOperations<OperationType::Read>(t, std::make_index_sequence<Reflex::TA_TypeInfo<T>::operationSize()> {});
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
+            callOperations(t, std::make_index_sequence<Reflex::TA_TypeInfo<T>::operationSize()> {});
             return *this;
         }
 
         template <EndianConvertedType T>
         TA_Serialization & operator << (T t)
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             if(TA_EndianConversion::isSystemLittleEndian())
                 t = TA_EndianConversion::swapEndian(t);
             m_fileSream.write(reinterpret_cast<const char*>(&t), sizeof(t));
@@ -98,6 +86,7 @@ namespace CoreAsync
         template <StdContainerType T, int64_t N = -1>
         TA_Serialization & operator << (const T &t)
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             *this << t.size();
             if constexpr(std::is_same_v<std::vector<typename T::value_type>, T> ||
                          std::is_same_v<std::list<typename T::value_type>, T> ||
@@ -172,18 +161,21 @@ namespace CoreAsync
         template <typename K, typename V>
         TA_Serialization & operator << (const std::pair<K, V> &pair)
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             return *this << pair.first << pair.second;
         }
 
         template <typename T, std::size_t Length = 1>
         TA_Serialization & operator << (const NormalPtr<T> &t)
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             return *this << Length << (*t);
         }
 
         template<typename T, int N>
         TA_Serialization & operator << (T (&a)[N])
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             for(int i = 0;i < N;++i)
             {
                 *this << a[i];
@@ -194,26 +186,28 @@ namespace CoreAsync
         template <typename T>
         TA_Serialization & operator << (EnumType<T> t)
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             *this << static_cast<uint8_t>(t);
             return *this;
         }
 
         TA_Serialization & operator << (std::nullptr_t)
         {
+            static_assert(OType == OperationType::Output, "The serilization type isn't WRITE");
             return *this;
         }
 
     private:
-        template <OperationType Operation, typename T>
+        template <typename T>
         constexpr void callOperations(const T &t, std::index_sequence<> = {})
         {
             return;
         }
 
-        template <OperationType Operation, typename T, std::size_t IDX0, std::size_t ...IDXS>
+        template <typename T, std::size_t IDX0, std::size_t ...IDXS>
         constexpr void callOperations (const T &t, std::index_sequence<IDX0, IDXS...> = {})
         {
-            if constexpr(Operation == OperationType::Read)
+            if constexpr(OType == OperationType::Output)
             {
                 *this << std::invoke(std::get<0>(Reflex::TA_TypeInfo<T>::template findPropertyOperation<IDX0>()), t);
                 callOperations(t, std::index_sequence<IDXS...> {});
@@ -224,10 +218,32 @@ namespace CoreAsync
             }
         }
 
+    private:
+        bool openFile(const std::string &filePath)
+        {
+            if(m_fileSream.is_open())
+            {
+                m_fileSream.close();
+            }
+            if constexpr(OType == OperationType::Input)
+            {
+                m_fileSream.open(filePath, std::ios::binary | std::ios::in);
+            }
+            else
+            {
+                m_fileSream.open(filePath, std::ios::binary | std::ios::out);
+            }
+            if (!m_fileSream || !m_fileSream.is_open())
+            {
+                CoreAsync::TA_CommonTools::debugInfo("Cannot open the file.\n");
+                return false;
+            }
+            return true;
+        }
 
 
     private:
-        std::ofstream m_fileSream;
+        std::fstream m_fileSream;
 
     };
 }
