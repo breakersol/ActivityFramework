633dc0b23e4e3f74f4af67d3a4c89d9f4d59ab81
Zhu Shuang <30600875+breakersol@users.noreply.github.com>
2024-09-02 02:56:04 +0800

    Update benchmark for serialization and deserialization on Windows
    Update benchmark for serialization and deserialization on Windows.

--- Benchmark/main.cpp
+++ Benchmark/main.cpp
@@ -1,18 +1,51 @@
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state)
-        std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+#include "Components/TA_Serialization.h"
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-    std::string x = "hello";
-    for (auto _ : state)
-        std::string copy(x);
+static void BM_Serialization(benchmark::State &state)
+{
+    // Data preparation
+    std::vector<int> vectorData(100000, 42);
+    std::list<int> listData(100000, 42);
+    std::map<int, double> mapData;
+    for (int i = 0; i < 100000; ++i)
+    {
+        mapData[i] = i * 0.5;
+    }
+
+    // Benchmark serialization
+    {
+        CoreAsync::TA_Serializer output("./test.afw");
+        for (auto _ : state) {
+            output << vectorData << listData << mapData;
+            output.flush();
+            output.close();
+        }
+    }
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_Serialization)->Iterations(1);
+
+static void BM_Deserialization(benchmark::State& state)
+{
+    // Data preparation
+    std::vector<int> vec;
+    std::list<int> lt;
+    std::map<int, double> md;
+
+    // Benchmark deserialization
+    {
+        CoreAsync::TA_Serializer<CoreAsync::BufferReader> input("./test.afw");
+        for (auto _ : state) {
+            input >> vec >> lt >> md;
+            input.close();
+
+            // To prevent the compiler from optimizing out the whole loop
+            benchmark::DoNotOptimize(vec);
+            benchmark::DoNotOptimize(lt);
+            benchmark::DoNotOptimize(md);
+        }
+    }
+}
+BENCHMARK(BM_Deserialization)->Iterations(1);
 
 BENCHMARK_MAIN();
